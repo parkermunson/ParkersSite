fe8099c1a39120aa2ac4393fc00ab9c0d03300f1
Parker Munson <parkermunson@Parkers-MacBook-Air.local>
2023-11-14 13:41:04 -0500

    Updating github repo with local files

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const div = document.querySelector(".text");
+const text = "Copywriter + Full-stack Web Developer";
+
+function textTypingEffect(element, text, i = 0) {
+    if (i === 0) {
+        element.textContent = " ";
+    }
+    element.textContent += text[i];
+
+    if (i === text.length - 1) {
+        return;
+    }
+
+    setTimeout(() => textTypingEffect(element, text, i + 1), 50);
+
+}
+
+textTypingEffect(div, text);
+
+console.log("Copywriter + Full-stack Web Developer")
